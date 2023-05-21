b126c2464df4b87fedfe0a6cc44aba641848cfed
dekapd99 <dekaprimatio@gmail.com>
2023-05-22 05:30:56 +0700

    Initial Commit

---
+++ B-Kuik Watch App/B_KuikApp.swift
@@ -0,0 +1,17 @@
+//
+//  B_KuikApp.swift
+//  B-Kuik Watch App
+//
+//  Created by Deka Primatio on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct B_Kuik_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ B-Kuik Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  B-Kuik Watch App
+//
+//  Created by Deka Primatio on 22/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
